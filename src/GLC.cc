d9596f7e620788d3cc8dc1e49b6598b8212693c4
Bruno Turcksin <bruno.turcksin@gmail.com>
2013-06-11 08:56:34 -0500

    Fix a typo in the license.

--- src/GLC.cc
+++ src/GLC.cc
@@ -1,7 +1,7 @@
-/* Copyright (c) 2013, Bruno Turcksin
+/* Copyright (c) 2013, Bruno Turcksin.
  *
  * This file is subject to the Modified BSD License and may not be distributed
- * without copyright and license information. Pleas refer to the file
+ * without copyright and license information. Please refer to the file
  * license.txt for the text and further information on this license.
  */
 
